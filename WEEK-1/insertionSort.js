2c9f97c2e72c0b9b3f47a216cf0453845b271bb7
Praveen7Tech <praveen.tech.js@gmail.com>
2025-04-11 23:04:18 +0530

    insertion sort

---
+++ WEEK-1/insertionSort.js
@@ -0,0 +1,48 @@
+
+// Insertion Sort 1
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+console.log(insertionSort([5, 3, 8, 4, 2]));
+
+// Insertion Sort Using While Loop
+
+function insertionSortWhile(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i], j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+console.log(insertionSortWhile([9, 1, 6, 3, 7]));
+
+
+// Descending Order Insertion Sort
+
+
+function insertionSortDescending(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i], j = i - 1;
+        while (j >= 0 && arr[j] < key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+console.log(insertionSortDescending([4, 2, 9, 1, 7]));
